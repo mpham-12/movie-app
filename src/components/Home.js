0e83817d19ee71cb57ba54370929606de60b5842
mpham-12 <martinp0512@gmail.com>
2022-10-17 16:45:07 -0400

    create homepage and searchbar component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import SearchBar from "./SearchBar";
+
+const Home = () => {
+  return ( 
+    <div>
+      <h1>movie app</h1>
+      <SearchBar/>
+    </div>
+   );
+}
+ 
+export default Home;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+const SearchBar = () => {
+  return ( 
+<form action="">
+  <input type="text" name="" id="" placeholder="search a movie..."/>
+  <button>search</button>
+</form>
+   );
+}
+ 
+export default SearchBar;
