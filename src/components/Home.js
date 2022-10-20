1c85ac50e5a69062e9c23e0042e4b2d6fa971ca2
mpham-12 <martinp0512@gmail.com>
2022-10-19 21:29:15 -0400

    remove .env and store api data into movie state

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,34 @@
 import axios from 'axios';
 import SearchBar from "./SearchBar";
 import GenresDrawer from "./GenresDrawer";
+import { useEffect, useState } from 'react';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-const getMoviesData = async()=>{
-const res = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`);
-
-const data = res.data;
-console.log('DATA', data)
-}
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
 
-getMoviesData();
+  useEffect(() => {
+    const getMoviesData = async () => {
+      const res = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}`);
+
+      const data = res.data;
+      console.log('DATA', data)
+      setMovies(data.results);
+    }
+
+    getMoviesData();
+  }, []);
+
+
   return (
     <div>
       <h1>PluggedIn</h1>
       <GenresDrawer />
       <SearchBar />
+
+      <h2>Trending Movies</h2>
+      { }
     </div>
   );
 }
