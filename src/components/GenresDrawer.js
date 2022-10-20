39aba7b5a97cd7fa88fee61e582fc433b213fb6f
mpham-12 <martinp0512@gmail.com>
2022-10-19 22:35:07 -0400

    render movie genres in drawer

--- src/components/GenresDrawer.js
+++ src/components/GenresDrawer.js
@@ -1,18 +1,34 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 
 export default function TemporaryDrawer() {
-  const [state, setState] = React.useState(false);
+  const [state, setState] = useState(false);
+  const [genres, setGenres] = useState([]);
+
+
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  useEffect(() => {
+    const getGenres = async () => {
+      const res = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`);
+
+      const data = res.data;
+      console.log('GENRES', data.genres)
+      setGenres(data.genres);
+    }
+
+    getGenres();
+
+  }, [])
+  console.log('genres', genres)
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -30,13 +46,10 @@ export default function TemporaryDrawer() {
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
+        {genres.map((genre) => (
+          <ListItem key={genre.id} disablePadding>
             <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText primary={genre.name} />
             </ListItemButton>
           </ListItem>
         ))}
@@ -46,17 +59,17 @@ export default function TemporaryDrawer() {
 
   return (
     <div>
+      <React.Fragment>
+        <Button onClick={toggleDrawer('left', true)}>Genres</Button>
+        <Drawer
+          anchor={'left'}
+          open={state['left']}
+          onClose={toggleDrawer('left', false)}
+        >
+          {list('left')}
+        </Drawer>
 
-        <React.Fragment>
-          <Button onClick={toggleDrawer('left', true)}>Genres</Button>
-          <Drawer
-            anchor={'left'}
-            open={state['left']}
-            onClose={toggleDrawer('left', false)}
-          >
-            {list('left')}
-          </Drawer>
-        </React.Fragment>
+      </React.Fragment>
     </div>
   );
 }
