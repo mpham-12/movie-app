8be7c3933f16b8e1210917f4babdfe81e0d01436
mpham-12 <martinp0512@gmail.com>
2022-10-23 17:13:46 -0400

    create component for trending movies on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,9 @@
-import axios from 'axios';
 import SearchBar from "./SearchBar";
 import GenresDrawer from "./GenresDrawer";
-import { useEffect, useState } from 'react';
-import Movie from './Movie';
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+import TrendingMovies from './TrendingMovies';
 
 const Home = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    const getMoviesData = async () => {
-      const res = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
-
-      const data = res.data;
-      console.log('DATA', data)
-      setMovies(data.results);
-    }
-
-    getMoviesData();
-  }, []);
-
 
   return (
     <div>
@@ -28,10 +11,7 @@ const Home = () => {
       <GenresDrawer />
       <SearchBar />
 
-      <h2>Trending Movies</h2>
-      {movies.map((movie) => {
-        return <Movie key={movie.id} title={movie.title} rating={movie.vote_average} image={movie.poster_path} />
-      })}
+<TrendingMovies/>
     </div>
   );
 }
---
+++ src/components/TrendingMovies.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Movie from './Movie';
+
+
+const TrendingMovies = () => {
+  const [movies, setMovies] = useState([]);
+
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  useEffect(() => {
+    const getMoviesData = async () => {
+      const res = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`);
+
+      const data = res.data;
+      console.log('DATA', data)
+      setMovies(data.results);
+    }
+
+    getMoviesData();
+  }, []);
+  return (
+    <div>
+      <h2>Trending Movies</h2>
+      <div className="moviesList">
+        {movies.map((movie) => {
+          return <Movie key={movie.id} title={movie.title} rating={movie.vote_average} image={movie.poster_path} />
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default TrendingMovies;
