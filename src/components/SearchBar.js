04b9fa3c40c4890886df4b3fdd47aa0cf4ccec1d
mpham-12 <martinp0512@gmail.com>
2022-10-27 21:39:25 -0400

    show search results on key change... NEED TO FIX

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,58 @@
 import * as React from 'react';
 import { Button, Input, InputLabel, InputAdornment, FormControl, Box } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import { useState, useEffect } from 'react';
+import SearchResults from './SearchResults';
 
 const SearchBar = (props) => {
+  const [searchResults, setSearchResults] = useState(false);
+  const [query, setQuery] = useState('');
 
+  const submitHandler = (e) => {
+    e.preventDefault()
+    setSearchResults(true);
+    console.log('FORMS UBMITTED???')
+
+    setQuery('');
+  }
+
+  const queryHandler = (e) => {
+    setQuery(e.target.value);
+  }
+
+  const closeSearchResults = () => {
+    setSearchResults(false);
+  }
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+  }
 
   return (
-    <Box sx={{ '& > :not(style)': { m: 1 } }}>
-      <FormControl variant="standard">
-        <InputLabel htmlFor="input-with-icon-adornment">
-          Find a Movie
-        </InputLabel>
-        <Input
-          id="input-with-icon-adornment"
-          startAdornment={
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          }
-        />
-        <Button variant="outlined" onClick={props.onSearch}>
-          Search
-        </Button>
-      </FormControl>
-    </Box>
+    <div>
+      <Box sx={{ '& > :not(style)': { m: 1 } }}>
+        <FormControl variant="standard" onSubmit={submitHandler}>
+          <InputLabel htmlFor="input-with-icon-adornment">
+            Find a Movie
+          </InputLabel>
+          <Input
+            id="input-with-icon-adornment"
+            startAdornment={
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            }
+            value={query}
+            onChange={queryHandler}
+          />
+          <Button variant="outlined" >
+            Search
+          </Button>
+        </FormControl>
+      </Box>
+
+      {searchResults && <SearchResults onClose={closeSearchResults} queryInput={query} />}
+    </div>
   );
 }
 
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import Movie from "./Movie";
+
+
 const SearchResults = (props) => {
+  const [queryResults, setQueryResults] = useState([])
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  console.log('props', props)
+  useEffect(() => {
+    const getQuery = async () => {
+      const res = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${props.queryInput}&page=1&include_adult=false`);
+
+      const data = res.data;
+      console.log('SEARCH RESULTS', data.results)
+      setQueryResults(data.results);
+    }
+    getQuery();
+  }, [])
   return (
     <div>
       <h2>
         SEARCH RESULTS
-
+        <div className="moviesList">
+        {queryResults.map((movie) => {
+          return <Movie key={movie.id} title={movie.title} rating={movie.vote_average} image={movie.poster_path} />
+        })}
+        </div>
         <button onClick={props.onClose}> CLOSE</button>
       </h2>
     </div>
